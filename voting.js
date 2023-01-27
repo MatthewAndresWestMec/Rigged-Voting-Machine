d0efb43967827d1d1b6a07ed372cb68f14749440
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2023-01-26 18:21:12 -0700

    finished main page need back end

---
+++ voting.js
@@ -0,0 +1,16 @@
+
+function animations(){
+
+}
+
+function voteCounted(){
+document.getElementById("directions").innerHTML = "Thank You For Voting Richard Nixon";
+console.log("vote counted")
+}
+
+
+// let cursorY = event.clientY;
+// let cursorX = event.clientX;
+// if(cursorX - McGovernX < 30 & cursorY - McGoverY < 30){
+//     console.log('you are close')
+// }
